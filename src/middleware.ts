bccfefd6ffa2fe78b735317737d967bc5720d537
ChiragChrg <chiruchirag2001@gmail.com>
2024-11-03 13:54:57 +0530

    Modified Middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,44 +1,50 @@
-import { publicRoutes, authRoutes, ApiAuthPrefix, DEFAULT_LOGIN_REDIRECT } from '@/lib/routes';
-import { NextRequest, NextResponse } from "next/server";
-import authConfig from "./auth.config";
-import NextAuth from "next-auth";
-import { getToken } from 'next-auth/jwt';
+// import { publicRoutes, authRoutes, ApiAuthPrefix, DEFAULT_LOGIN_REDIRECT } from '@/lib/routes';
+// import { NextRequest, NextResponse } from "next/server";
+// import authConfig from "./auth.config";
+// import NextAuth from "next-auth";
+// import { getToken } from 'next-auth/jwt';
 
-const { auth } = NextAuth(authConfig)
+// const { auth } = NextAuth(authConfig)
 
-export default auth(async function middleware(req: NextRequest) {
-    const { nextUrl } = req;
-    console.log("\nMiddlewareToken : ", { secret: process.env.AUTH_SECRET, req }, await getToken({ req: req, secret: process.env.AUTH_SECRET }))
-    const isLoggedIn = !!await getToken({ req: req, secret: process.env.AUTH_SECRET })
-    const isApiAuthRoute = nextUrl.pathname.startsWith(ApiAuthPrefix);
-    const isPublicRoute = publicRoutes.some(route => nextUrl.pathname === route);
-    const isAuthRoute = authRoutes.includes(nextUrl.pathname);
+// export default auth(async function middleware(req: NextRequest) {
+//     const { nextUrl } = req;
+//     console.log("\nMiddlewareToken : ", { secret: process.env.AUTH_SECRET, req }, await getToken({ req: req, secret: process.env.AUTH_SECRET }))
+//     const isLoggedIn = !!await getToken({ req: req, secret: process.env.AUTH_SECRET })
+//     const isApiAuthRoute = nextUrl.pathname.startsWith(ApiAuthPrefix);
+//     const isPublicRoute = publicRoutes.some(route => nextUrl.pathname === route);
+//     const isAuthRoute = authRoutes.includes(nextUrl.pathname);
 
-    console.log("\nMiddleware : ", { nextUrl, isLoggedIn, isApiAuthRoute, isPublicRoute, isAuthRoute })
+//     console.log("\nMiddleware : ", { nextUrl, isLoggedIn, isApiAuthRoute, isPublicRoute, isAuthRoute })
 
-    if (isApiAuthRoute) {
-        console.log("M_Redirect : isApiAuthRoute")
-        return NextResponse.next();
-    }
-    if (isAuthRoute) {
-        if (isLoggedIn) {
-            console.log("M_Redirect : isLoggedIn")
-            return NextResponse.redirect(new URL(DEFAULT_LOGIN_REDIRECT, nextUrl));
-        }
+//     if (isApiAuthRoute) {
+//         console.log("M_Redirect : isApiAuthRoute")
+//         return NextResponse.next();
+//     }
+//     if (isAuthRoute) {
+//         if (isLoggedIn) {
+//             console.log("M_Redirect : isLoggedIn")
+//             return NextResponse.redirect(new URL(DEFAULT_LOGIN_REDIRECT, nextUrl));
+//         }
 
-        console.log("M_Redirect : isAuthRoute")
-        return NextResponse.next();
-    }
-    if (!isLoggedIn && !isPublicRoute) {
-        console.log("M_Redirect : isAuthRoute")
-        return NextResponse.redirect(new URL('/login', nextUrl));
-    }
-    return NextResponse.next();
-});
+//         console.log("M_Redirect : isAuthRoute")
+//         return NextResponse.next();
+//     }
+//     if (!isLoggedIn && !isPublicRoute) {
+//         console.log("M_Redirect : isAuthRoute")
+//         return NextResponse.redirect(new URL('/login', nextUrl));
+//     }
+//     return NextResponse.next();
+// });
+
+// export const config = {
+//     matcher: ['/((?!api|_next/static|_next/image|.*\\.png$).*)'],
+//     unstable_allowDynamic: [
+//         '/node_modules/mongoose/dist/browser.umd.js',
+//     ],
+// };
+
+export { auth as middleware } from "@/auth"
 
 export const config = {
-    matcher: ['/((?!api|_next/static|_next/image|.*\\.png$).*)'],
-    unstable_allowDynamic: [
-        '/node_modules/mongoose/dist/browser.umd.js',
-    ],
-};
+    matcher: ["/((?!api|_next/static|_next/image|favicon.ico).*)"],
+}
