2f0392f3d578cb219b03bc4043a8033a4b2e0047
ChiragChrg <chiruchirag2001@gmail.com>
2024-11-03 14:21:22 +0530

    Modified Middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,40 +1,39 @@
-// import { publicRoutes, authRoutes, ApiAuthPrefix, DEFAULT_LOGIN_REDIRECT } from '@/lib/routes';
-// import { NextRequest, NextResponse } from "next/server";
-// import authConfig from "./auth.config";
-// import NextAuth from "next-auth";
-// import { getToken } from 'next-auth/jwt';
+import { protectedRoutes, authRoutes, DEFAULT_LOGIN_REDIRECT } from '@/lib/routes';
+import { NextRequest, NextResponse } from "next/server";
+import authConfig from "./auth.config";
+import NextAuth from "next-auth";
 
-// const { auth } = NextAuth(authConfig)
+const { auth } = NextAuth(authConfig)
+// export { auth } from "@/auth"
 
-// export default auth(async function middleware(req: NextRequest) {
-//     const { nextUrl } = req;
-//     console.log("\nMiddlewareToken : ", { secret: process.env.AUTH_SECRET, req }, await getToken({ req: req, secret: process.env.AUTH_SECRET }))
-//     const isLoggedIn = !!await getToken({ req: req, secret: process.env.AUTH_SECRET })
-//     const isApiAuthRoute = nextUrl.pathname.startsWith(ApiAuthPrefix);
-//     const isPublicRoute = publicRoutes.some(route => nextUrl.pathname === route);
-//     const isAuthRoute = authRoutes.includes(nextUrl.pathname);
+export default async function middleware(req: NextRequest) {
+    const session = await auth();
+    console.log("\nMiddlewre Session:", session)
+    // export default auth(async function middleware(req: NextRequest) {
+    const { nextUrl } = req;
+    //     console.log("\nMiddlewareToken : ", { secret: process.env.AUTH_SECRET, req }, await getToken({ req: req, secret: process.env.AUTH_SECRET }))
+    const isLoggedIn = !!session
+    const isProtectedRoutes = protectedRoutes.some(route => nextUrl.pathname === route);
+    const isAuthRoute = authRoutes.includes(nextUrl.pathname);
 
-//     console.log("\nMiddleware : ", { nextUrl, isLoggedIn, isApiAuthRoute, isPublicRoute, isAuthRoute })
+    console.log("\nMiddleware : ", { nextUrl: nextUrl.pathname, isLoggedIn, isProtectedRoutes, isAuthRoute })
 
-//     if (isApiAuthRoute) {
-//         console.log("M_Redirect : isApiAuthRoute")
-//         return NextResponse.next();
-//     }
-//     if (isAuthRoute) {
-//         if (isLoggedIn) {
-//             console.log("M_Redirect : isLoggedIn")
-//             return NextResponse.redirect(new URL(DEFAULT_LOGIN_REDIRECT, nextUrl));
-//         }
+    if (isAuthRoute) {
+        if (isLoggedIn) {
+            console.log("M_Redirect : isLoggedIn")
+            return NextResponse.redirect(new URL(DEFAULT_LOGIN_REDIRECT, nextUrl));
+        }
 
-//         console.log("M_Redirect : isAuthRoute")
-//         return NextResponse.next();
-//     }
-//     if (!isLoggedIn && !isPublicRoute) {
-//         console.log("M_Redirect : isAuthRoute")
-//         return NextResponse.redirect(new URL('/login', nextUrl));
-//     }
-//     return NextResponse.next();
-// });
+        console.log("M_Redirect : isAuthRoute")
+        return NextResponse.next();
+    }
+    if (!isLoggedIn && isProtectedRoutes) {
+        console.log("M_Redirect : isAuthRoute")
+        return NextResponse.redirect(new URL('/login', nextUrl));
+    }
+    return NextResponse.next();
+    // });
+}
 
 // export const config = {
 //     matcher: ['/((?!api|_next/static|_next/image|.*\\.png$).*)'],
@@ -43,7 +42,25 @@
 //     ],
 // };
 
-export { auth as middleware } from "@/auth"
+// 
+// 
+
+// export { auth } from "@/auth"
+
+// export default async function middleware(request: NextRequest) {
+//     const session = await auth();
+
+//     const isProtected = protectedRoutes.some((route) =>
+//       request.nextUrl.pathname.startsWith(route)
+//     );
+
+//     if (!session && isProtected) {
+//       const absoluteURL = new URL("/", request.nextUrl.origin);
+//       return NextResponse.redirect(absoluteURL.toString());
+//     }
+
+//     return NextResponse.next();
+//   }
 
 export const config = {
     matcher: ["/((?!api|_next/static|_next/image|favicon.ico).*)"],
