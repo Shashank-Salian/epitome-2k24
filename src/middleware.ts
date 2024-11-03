f4f4151aa9f4d6e3176e4a955f9d80daae34b301
ChiragChrg <chiruchirag2001@gmail.com>
2024-11-03 14:25:47 +0530

    FIx: Resolved Auth Middleware Issues. Auth Redirection Working 🚀

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,22 +1,19 @@
-import { protectedRoutes, authRoutes, DEFAULT_LOGIN_REDIRECT } from '@/lib/routes';
-import { NextRequest, NextResponse } from "next/server";
-import authConfig from "./auth.config";
 import NextAuth from "next-auth";
+import authConfig from "./auth.config";
+import { NextRequest, NextResponse } from "next/server";
+import { protectedRoutes, authRoutes, DEFAULT_LOGIN_REDIRECT } from '@/lib/routes';
 
 const { auth } = NextAuth(authConfig)
-// export { auth } from "@/auth"
 
 export default async function middleware(req: NextRequest) {
     const session = await auth();
-    console.log("\nMiddlewre Session:", session)
-    // export default auth(async function middleware(req: NextRequest) {
+
     const { nextUrl } = req;
-    //     console.log("\nMiddlewareToken : ", { secret: process.env.AUTH_SECRET, req }, await getToken({ req: req, secret: process.env.AUTH_SECRET }))
     const isLoggedIn = !!session
     const isProtectedRoutes = protectedRoutes.some(route => nextUrl.pathname === route);
     const isAuthRoute = authRoutes.includes(nextUrl.pathname);
 
-    console.log("\nMiddleware : ", { nextUrl: nextUrl.pathname, isLoggedIn, isProtectedRoutes, isAuthRoute })
+    // console.log("\nMiddleware : ", { nextUrl: nextUrl.pathname, isLoggedIn, isProtectedRoutes, isAuthRoute })
 
     if (isAuthRoute) {
         if (isLoggedIn) {
@@ -32,35 +29,8 @@ export default async function middleware(req: NextRequest) {
         return NextResponse.redirect(new URL('/login', nextUrl));
     }
     return NextResponse.next();
-    // });
 }
 
-// export const config = {
-//     matcher: ['/((?!api|_next/static|_next/image|.*\\.png$).*)'],
-//     unstable_allowDynamic: [
-//         '/node_modules/mongoose/dist/browser.umd.js',
-//     ],
-// };
-
-// 
-// 
-
-// export { auth } from "@/auth"
-
-// export default async function middleware(request: NextRequest) {
-//     const session = await auth();
-
-//     const isProtected = protectedRoutes.some((route) =>
-//       request.nextUrl.pathname.startsWith(route)
-//     );
-
-//     if (!session && isProtected) {
-//       const absoluteURL = new URL("/", request.nextUrl.origin);
-//       return NextResponse.redirect(absoluteURL.toString());
-//     }
-
-//     return NextResponse.next();
-//   }
 
 export const config = {
     matcher: ["/((?!api|_next/static|_next/image|favicon.ico).*)"],
