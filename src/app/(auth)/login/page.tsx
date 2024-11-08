fd1ddb96c3b530d74d14d756dd35d3336222900e
ChiragChrg <chiruchirag2001@gmail.com>
2024-11-09 01:18:28 +0530

    Minor Ui Changes

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -4,13 +4,9 @@ import Header from '@/components/CustomUI/Header'
 
 const Login = () => {
     return (
-        <main className='w-full h-screen flex_center flex-col'>
-            <Header />
-
-            <div className="w-full h-full flex_center">
-                <LoginForm />
-            </div>
-        </main>
+        <div className="w-full h-full flex_center">
+            <LoginForm />
+        </div>
     )
 }
 
