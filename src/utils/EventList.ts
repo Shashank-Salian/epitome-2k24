391c283b7a1fcac763f645a835118c96c15a418c
Shashank-Salian <shashanksalian96@gmail.com>
2024-11-10 16:43:38 +0530

    added all the events and models

--- src/utils/EventList.ts
+++ src/utils/EventList.ts
@@ -5,7 +5,7 @@ type EventType = {
     name: string;
     phone?: string;
     picture?: string;
-  };
+  }[];
   eventHeads: {
     name: string;
     phone: string;
@@ -20,9 +20,11 @@ const EventList: EventType = [
   {
     title: "Binary Universe",
     eventName: "Coding",
-    coordinator: {
-      name: "Dr Santhosh B",
-    },
+    coordinator: [
+      {
+        name: "Dr Santhosh B",
+      },
+    ],
     eventHeads: [
       { name: "Vinayaka D", phone: "+91 9483121037" },
       { name: "Sachin Hadimani", phone: "+91 9110232822" },
@@ -41,9 +43,11 @@ const EventList: EventType = [
   {
     title: "Quasar Invictus",
     eventName: "IT Manager",
-    coordinator: {
-      name: "Dr Jeevan Pinto",
-    },
+    coordinator: [
+      {
+        name: "Dr Jeevan Pinto",
+      },
+    ],
     eventHeads: [
       {
         name: "Riya G",
@@ -68,10 +72,12 @@ const EventList: EventType = [
   {
     title: "Matrix Minds",
     eventName: "IT Quiz",
-    coordinator: {
-      name: "Dr. Ruban S",
-      phone: "+91 9741965134",
-    },
+    coordinator: [
+      {
+        name: "Dr. Ruban S",
+        phone: "+91 9741965134",
+      },
+    ],
     eventHeads: [
       {
         name: "Sharath Acharya",
@@ -95,9 +101,11 @@ const EventList: EventType = [
   {
     title: "Cosmic Webcraft",
     eventName: "Web Designing",
-    coordinator: {
-      name: "Dr Srinivas B.L",
-    },
+    coordinator: [
+      {
+        name: "Dr Srinivas B.L",
+      },
+    ],
     eventHeads: [
       {
         name: "Harishri",
@@ -121,9 +129,11 @@ const EventList: EventType = [
   {
     title: "Rhythmic Resonance",
     eventName: "Dance",
-    coordinator: {
-      name: "Dr Rakesh Kumar B",
-    },
+    coordinator: [
+      {
+        name: "Dr Rakesh Kumar B",
+      },
+    ],
     eventHeads: [
       {
         name: "Shravya S",
@@ -153,10 +163,12 @@ const EventList: EventType = [
   {
     title: "Battle of the Bytes",
     eventName: "Debate",
-    coordinator: {
-      name: "Mrs. Annapoorna Shetty",
-      phone: "+91 9731850981",
-    },
+    coordinator: [
+      {
+        name: "Mrs. Annapoorna Shetty",
+        phone: "+91 9731850981",
+      },
+    ],
     eventHeads: [
       {
         name: "Preetham N",
@@ -181,10 +193,12 @@ const EventList: EventType = [
   {
     title: "Cosmic Creatives",
     eventName: "Mad Ad",
-    coordinator: {
-      name: "Ms Nausheeda B.S",
-      phone: "+91 9591520632",
-    },
+    coordinator: [
+      {
+        name: "Ms Nausheeda B.S",
+        phone: "+91 9591520632",
+      },
+    ],
     eventHeads: [
       {
         name: "Shravya C",
@@ -210,20 +224,182 @@ const EventList: EventType = [
     description:
       "A simulation of real-world business challenges, where participants showcase their management, leadership, and decision-making skills through a series of tech-oriented tasks.",
   },
+  {
+    title: " Meme Mania",
+    eventName: "AI Meme Generator",
+    coordinator: [
+      {
+        name: "Mrs Manimozhi R",
+        phone: "+91 9449365781 ",
+      },
+    ],
+    eventHeads: [
+      {
+        name: "Yashaswi",
+        phone: "+91 8748966783 ",
+      },
+      {
+        name: "Sonali",
+        phone: "+91 9746278256",
+      },
+      {
+        name: "Shreya",
+        phone: "+91 6364218734 ",
+      },
+    ],
+    rules: [
+      "No of participant is 1",
+      "Participant must get their own laptop. ",
+      "There will be 2 rounds.",
+      "It will be performed using AI tools and elimination will done in round 1.",
+      "No usage of offensive words, Should not hurt any religious beliefs. ",
+    ],
+    description: "",
+    modelName: "meme_gen.glb",
+  },
+  {
+    title: "Eliptica",
+    eventName: "Treasure Hunt",
+    coordinator: [
+      {
+        name: "Mr Aravinda Prabhu S",
+        phone: "+91 9986263916 ",
+      },
+    ],
+    eventHeads: [
+      {
+        name: "Soujanya",
+        phone: "+91 9061752785 ",
+      },
+      {
+        name: "Pawana",
+        phone: "+91 9731957420",
+      },
+    ],
+    rules: [
+      "Teams consist of two members.",
+      "Event rounds will be announced on the spot, but it's advisable to be prepared with puzzle-solving skills, knowledge of riddles, and technologies. ",
+      "Missing any one round or causing damage to property will result in team disqualification.",
+      "Elimination will be based on participation and time duration.",
+    ],
+    description: "",
+    modelName: "treasure_hunt.glb",
+  },
+  {
+    title: "Galactic Frames",
+    eventName: "Videography and Photography",
+    coordinator: [
+      {
+        name: "Ms. Vanitha",
+        phone: "+91 9964412532",
+      },
+    ],
+    eventHeads: [
+      {
+        name: "Vinayak V K",
+        phone: "+91 8590483586 ",
+      },
+      {
+        name: "Maeve Fernandes",
+        phone: "+91 8308756771",
+      },
+    ],
+    rules: [
+      "There is only one participant.",
+      "The participant has to know how to edit videos and take pictures ",
+      "On the day of the event, the theme will be provided.",
+      "Drones are not permitted.",
+      "Bring your own USB cords and any relevant devices.",
+      "Bring your own laptops. cameras, smartphones.",
+      "It is forbidden to edit using AI websites and software.",
+    ],
+    description: "",
+    modelName: "photography.glb",
+  },
+  {
+    title: "Ultimate Showdown",
+    eventName: "Gaming",
+    coordinator: [
+      {
+        name: "Dr Shine P Varkey",
+        phone: "+91 9497492972 ",
+      },
+    ],
+    eventHeads: [
+      {
+        name: "Alister Dsouza",
+        phone: "+91 8073629738",
+      },
+      {
+        name: "Vivek Pattar",
+        phone: "+91 9916032250",
+      },
+    ],
+    rules: [
+      "Each team can have maximum of 4 members",
+      "All the contesting members are to be present in the venue before the game starts from each team, only one group can perform. ",
+      "All the teams are requested to use their own data connection.",
+      "The coordinators decision is final",
+      "Account level should be above 25",
+      "Map: Erangel, Sanhok, Miramar, Vikendi. ",
+      "Room ID and password will be provided at the event.",
+      "No use of hacks",
+      "No use of triggers (Can Use Finger Gloves)",
+      "Screenshot the results after each match.",
+      "No using emergency pickup and self-revive kit",
+    ],
+    description: "",
+    modelName: "gaming.glb",
+  },
+  {
+    title: "Surprise Event",
+    eventName: "SURPRISE",
+    coordinator: [
+      {
+        name: "Ms. Christina",
+        phone: "+91 9606582375",
+      },
+      {
+        name: "Ms. Shilpa",
+        phone: "+91 8105324479",
+      },
+      {
+        name: "Ms. Gana",
+        phone: "+91 9995259225",
+      },
+    ],
+    eventHeads: [
+      {
+        name: "Nireeksha M",
+        phone: "+91 8197652325",
+      },
+      {
+        name: "Meghna",
+        phone: "+91 8494998893",
+      },
+    ],
+    rules: [
+      "Team consists of 2 members.",
+      "No mobile phones or laptops allowed.",
+      "Additional rules will be announced at the event.",
+    ],
+    description: "",
+    modelName: "surprise.glb",
+  },
 ];
 
 export default EventList;
 
 // Event Register List
 export type EventRegType = {
-  title: string,
-  category: string,
+  title: string;
+  category: string;
   participants: {
     name: string;
     phone: string;
-  }[]
-  participantCount: number,
-}
+  }[];
+  participantCount: number;
+};
 
 export const eventRegisterList: EventRegType[] = [
   {
@@ -232,12 +408,12 @@ export const eventRegisterList: EventRegType[] = [
     participants: [
       {
         name: "",
-        phone: ""
+        phone: "",
       },
       {
         name: "",
-        phone: ""
-      }
+        phone: "",
+      },
     ],
     participantCount: 2,
   },
@@ -247,24 +423,24 @@ export const eventRegisterList: EventRegType[] = [
     participants: [
       {
         name: "",
-        phone: ""
+        phone: "",
       },
       {
         name: "",
-        phone: ""
+        phone: "",
       },
       {
         name: "",
-        phone: ""
+        phone: "",
       },
       {
         name: "",
-        phone: ""
+        phone: "",
       },
       {
         name: "",
-        phone: ""
-      }
+        phone: "",
+      },
     ],
     participantCount: 5,
   },
@@ -274,8 +450,8 @@ export const eventRegisterList: EventRegType[] = [
     participants: [
       {
         name: "",
-        phone: ""
-      }
+        phone: "",
+      },
     ],
     participantCount: 1,
   },
@@ -285,12 +461,12 @@ export const eventRegisterList: EventRegType[] = [
     participants: [
       {
         name: "",
-        phone: ""
+        phone: "",
       },
       {
         name: "",
-        phone: ""
-      }
+        phone: "",
+      },
     ],
     participantCount: 2,
   },
@@ -300,8 +476,8 @@ export const eventRegisterList: EventRegType[] = [
     participants: [
       {
         name: "",
-        phone: ""
-      }
+        phone: "",
+      },
     ],
     participantCount: 1,
   },
@@ -311,8 +487,8 @@ export const eventRegisterList: EventRegType[] = [
     participants: [
       {
         name: "",
-        phone: ""
-      }
+        phone: "",
+      },
     ],
     participantCount: 1,
   },
@@ -322,20 +498,20 @@ export const eventRegisterList: EventRegType[] = [
     participants: [
       {
         name: "",
-        phone: ""
+        phone: "",
       },
       {
         name: "",
-        phone: ""
+        phone: "",
       },
       {
         name: "",
-        phone: ""
+        phone: "",
       },
       {
         name: "",
-        phone: ""
-      }
+        phone: "",
+      },
     ],
     participantCount: 4,
   },
@@ -345,12 +521,12 @@ export const eventRegisterList: EventRegType[] = [
     participants: [
       {
         name: "",
-        phone: ""
+        phone: "",
       },
       {
         name: "",
-        phone: ""
-      }
+        phone: "",
+      },
     ],
     participantCount: 2,
   },
@@ -360,20 +536,20 @@ export const eventRegisterList: EventRegType[] = [
     participants: [
       {
         name: "",
-        phone: ""
+        phone: "",
       },
       {
         name: "",
-        phone: ""
+        phone: "",
       },
       {
         name: "",
-        phone: ""
+        phone: "",
       },
       {
         name: "",
-        phone: ""
-      }
+        phone: "",
+      },
     ],
     participantCount: 4,
   },
@@ -383,12 +559,12 @@ export const eventRegisterList: EventRegType[] = [
     participants: [
       {
         name: "",
-        phone: ""
+        phone: "",
       },
       {
         name: "",
-        phone: ""
-      }
+        phone: "",
+      },
     ],
     participantCount: 2,
   },
@@ -398,8 +574,8 @@ export const eventRegisterList: EventRegType[] = [
     participants: [
       {
         name: "",
-        phone: ""
-      }
+        phone: "",
+      },
     ],
     participantCount: 1,
   },
@@ -409,13 +585,13 @@ export const eventRegisterList: EventRegType[] = [
     participants: [
       {
         name: "",
-        phone: ""
+        phone: "",
       },
       {
         name: "",
-        phone: ""
-      }
+        phone: "",
+      },
     ],
     participantCount: 2,
-  }
-]
+  },
+];
