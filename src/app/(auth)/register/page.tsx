8c55c716a06d66af21fb95e0a816056e4f7338e5
ChiragChrg <chiruchirag2001@gmail.com>
2024-11-22 09:28:55 +0530

    misc: Disabled Epitome Event Registration

--- src/app/(auth)/register/page.tsx
+++ src/app/(auth)/register/page.tsx
@@ -4,7 +4,8 @@ import RegisterForm from "@/components/Forms/RegisterForm"
 const Register = () => {
     return (
         <div className="w-full min-h-full flex_center pt-5 pb-16">
-            <RegisterForm />
+            {/* <RegisterForm /> */}
+            <h1 className='text-[5em] mt-52 font-oxanium'>Epitome Registration has Ended!</h1>
         </div>
     )
 }
