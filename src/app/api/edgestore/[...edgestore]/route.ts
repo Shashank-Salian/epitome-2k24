622aac30a258615ce1d5da8da1e7f83e50558c17
Shashank-Salian <shashanksalian96@gmail.com>
2025-01-23 13:37:34 +0530

    My showcase deploy

--- src/app/api/edgestore/[...edgestore]/route.ts
+++ src/app/api/edgestore/[...edgestore]/route.ts
@@ -1,16 +1,16 @@
-import { initEdgeStore } from '@edgestore/server';
-import { createEdgeStoreNextHandler } from '@edgestore/server/adapters/next/app';
+import { initEdgeStore } from "@edgestore/server";
+import { createEdgeStoreNextHandler } from "@edgestore/server/adapters/next/app";
 
 const es = initEdgeStore.create();
 
 const edgeStoreRouter = es.router({
-    publicImages: es.imageBucket({
-        maxSize: 1024 * 1024 * 10,
-    })
+  publicImages: es.imageBucket({
+    maxSize: 1024 * 1024 * 10,
+  }),
 });
 
 const handler = createEdgeStoreNextHandler({
-    router: edgeStoreRouter,
+  router: edgeStoreRouter,
 });
 
 export { handler as GET, handler as POST };
